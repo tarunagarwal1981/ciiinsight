8132d5a286e9a908222fc75e1bf73f3f922ed0d2
Tarun Agarwal <49944511+tarunagarwal1981@users.noreply.github.com>
2024-10-17 22:17:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { ThemeProvider, createTheme, CssBaseline } from '@material-ui/core';
 
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
